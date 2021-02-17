c256646088e9a063aa2e07f99ecbab357f5bb52e
YAMM25 <36430354+YAMM25@users.noreply.github.com>
2021-02-17 18:13:42 -0500

    End the CRUD in memory

--- src/App.js
+++ src/App.js
@@ -7,12 +7,25 @@ function App() {
   const [tasks, setTasks] = useState([])
   const [editMode, setEditMode] = useState(false)
   const [id, setId] = useState("")
+  const [error, setError] = useState(null)
+
+  const validForm = () => {
+    let isValid = true
+    setError(null)
+
+    if (isEmpty(task)){
+      setError("Debes ingresar una tarea.")
+      isValid = false
+    }
+
+    return isValid
+  }
 
   const addTask = (e) => {
     e.preventDefault()
-    if (isEmpty(task)){
-      console.log("Task empty")
-      return
+
+    if (!validForm()) {
+      return 
     }
 
     const newTask = {
@@ -26,9 +39,9 @@ function App() {
 
   const saveTask = (e) => {
     e.preventDefault()
-    if (isEmpty(task)){
-      console.log("Task empty")
-      return
+   
+    if (!validForm()) {
+      return 
     }
 
     const editedTasks = tasks.map(item => item.id === id ? {id, name: task} : item)
@@ -58,7 +71,7 @@ function App() {
           <h4 className="text-center">Lista de Tareas</h4>
           {
             size(tasks) === 0 ? (
-                <h5 className="text-center">Aún no hay tareas programadas.</h5>
+                <li className="list-group-item">Aún no hay tareas programadas.</li>
             ) : (
               <ul className="list-group">
                 {
@@ -90,6 +103,9 @@ function App() {
             { editMode ? "Modificar Tarea" : "Agregar Tarea" }
           </h4>  
           <form onSubmit={ editMode ? saveTask : addTask }>
+           {
+              error && <span className="text-danger">{error}</span>
+            } 
             <input
               type="text"
               className="form-control mb-2"
